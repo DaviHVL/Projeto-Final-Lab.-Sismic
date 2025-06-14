8f28dc84d35f024b296d20ef7c49a0109ebe680c
DaviHVL <davihvl@gmail.com>
2025-06-14 11:16:52 -0300

    feat: adição do mecanismo de temporização ao projeto

--- Debug/projeto_final.d
+++ Debug/projeto_final.d
@@ -5,6 +5,17 @@ projeto_final.obj: C:/ti/ccs2010/ccs/ccs_base/msp430/include/msp430f5529.h
 projeto_final.obj: C:/ti/ccs2010/ccs/ccs_base/msp430/include/in430.h
 projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
 projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+projeto_final.obj: C:/ti/ccs2010/ccs/ccs_base/msp430/include/msp430.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+projeto_final.obj: C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
 
 ../projeto_final.c:
 
@@ -15,4 +26,26 @@ C:/ti/ccs2010/ccs/ccs_base/msp430/include/in430.h:
 C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
 
 C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs2010/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs2010/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
 
--- projeto_final.c
+++ projeto_final.c
@@ -1,8 +1,18 @@
 #include <msp430f5529.h>
+#include <msp430.h>
+#include <stdint.h>
 
 #define LEDRGB_RED BIT4
 #define LEDRGB_GREEN BIT4
 #define LEDRGB_BLUE BIT2
+
+uint8_t operating_mode = 1; // 1: Estudo e 0: Descanso
+
+uint8_t active_time = 1; // Tempo de Estudo Total em Minutos
+uint8_t rest_time = 1;   // Tempo de Descanso Total em Minutos
+
+volatile uint16_t counting_active_time = 0; // Tempo de Estudo Passado em Segundos
+volatile uint16_t counting_rest_time = 0;   // Tempo de Descanso Passado em Segundos
 
 void main(void){
   WDTCTL = WDTPW | WDTHOLD;  // Travar watchdog 
@@ -16,7 +26,35 @@ void main(void){
   P1DIR |= LEDRGB_BLUE;
   P1SEL &= ~(LEDRGB_BLUE);
 
+  P2OUT &= ~(LEDRGB_RED);
+  P1OUT &= ~(LEDRGB_GREEN);
+  P1OUT &= ~(LEDRGB_BLUE);
+
+  TA0CTL = 	TASSEL__ACLK | MC__UP;
+	TA0CCR0 = 32768 - 1;				// Conta 1 segundo
+  TA0CCTL0 = CCIE;
+
+  __enable_interrupt(); 
   for(;;){
+    P1OUT |= LEDRGB_GREEN;
+    while((active_time*60) != counting_active_time);
+    counting_active_time = 0;
+    operating_mode = 0;
+    P1OUT &= ~(LEDRGB_GREEN);
+    P2OUT |= LEDRGB_RED;
+    while((rest_time*60) != counting_rest_time);
+    counting_rest_time = 0;
+    operating_mode = 1;
+    P2OUT &= ~(LEDRGB_RED);
+
   }
 }
 
+# pragma vector = TIMER0_A0_VECTOR
+__interrupt void TA0CCR0_ISR(){
+  if (operating_mode){
+    counting_active_time += 1;
+  } else{
+    counting_rest_time += 1;
+  }
+}
